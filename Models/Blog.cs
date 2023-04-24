4223a5572b4dfd72d93967f4bd558ba4583a64c0
Dave Thompson <davethompson59@aol.com>
2023-04-24 17:01:03 +0100

    Edited the scaffolded Blogs Index, Create, Edit, Details, & Delete Views

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -44,6 +44,7 @@ namespace TheITBlog.Models
         public IFormFile Image { get; set; }
 
         //Navigation properties
+        [Display(Name="Author")]
         public virtual BlogUser BlogUser { get; set; }
         public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
 
