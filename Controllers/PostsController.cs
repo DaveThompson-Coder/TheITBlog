89222e533a574a7759d8412b1f1b301b6eee416f
Dave Thompson <davethompson59@aol.com>
2023-05-02 23:42:03 +0100

    Added function to associate posts with tags, detect if tags are null or duplicated, and use Sweet Alerts

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -17,14 +18,18 @@ namespace TheITBlog.Controllers
         private readonly ApplicationDbContext _context;
         private readonly ISlugService _slugService;
         private readonly IImageService _imageService;
+        private readonly UserManager<BlogUser> _userManager;
 
         public PostsController(ApplicationDbContext context,
                                ISlugService slugService,
-                               IImageService imageService)
+                               IImageService imageService,
+                               UserManager<BlogUser> userManager)
+
         {
             _context = context;
             _slugService = slugService;
             _imageService = imageService;
+            _userManager = userManager;
         }
 
         // GET: Posts/Index
@@ -36,9 +41,9 @@ namespace TheITBlog.Controllers
         }
 
         // GET: Posts/Details
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(string slug)
         {
-            if (id == null)
+            if (string.IsNullOrEmpty(slug))
             {
                 return NotFound();
             }
@@ -46,7 +51,8 @@ namespace TheITBlog.Controllers
             var post = await _context.Posts
                 .Include(p => p.Blog)
                 .Include(p => p.BlogUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .Include(p => p.Tags)
+                .FirstOrDefaultAsync(m => m.Slug == slug);
             if (post == null)
             {
                 return NotFound();
@@ -74,6 +80,9 @@ namespace TheITBlog.Controllers
             {
                 post.Created = DateTime.Now;
 
+                var authorId = _userManager.GetUserId(User);
+                post.BlogUserId = authorId;
+
                 //Use the _imageService to store the incoming user specified image
                 post.ImageData = await _imageService.EncodeImageAsync(post.Image);
                 post.ContentType = _imageService.ContentType(post.Image);
@@ -91,6 +100,20 @@ namespace TheITBlog.Controllers
 
                 _context.Add(post);
                 await _context.SaveChangesAsync();
+
+                //How do I loop over the incoming list of string?
+                foreach(var tagText in tagValues)
+                {
+                    _context.Add(new Tag()
+                    {
+                        PostId = post.Id,
+                        BlogUserId = authorId,
+                        Text = tagText
+                    });
+                }
+
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
 
@@ -107,12 +130,14 @@ namespace TheITBlog.Controllers
                 return NotFound();
             }
 
-            var post = await _context.Posts.FindAsync(id);
+            var post = await _context.Posts.Include(p=> p.Tags).FirstOrDefaultAsync(p => p.Id == id);
+
             if (post == null)
             {
                 return NotFound();
             }
             ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Name", post.BlogId);
+            ViewData["TagValues"] = string.Join(",", post.Tags.Select(t => t.Text));
 
             return View(post);
         }
@@ -122,7 +147,7 @@ namespace TheITBlog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus")] Post post, IFormFile newImage)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus")] Post post, IFormFile newImage, List<string> tagValues)
         {
             if (id != post.Id)
             {
@@ -133,7 +158,7 @@ namespace TheITBlog.Controllers
             {
                 try
                 {
-                    var newPost = await _context.Posts.FindAsync(post.Id);
+                    var newPost = await _context.Posts.Include(p => p.Tags).FirstOrDefaultAsync(p => p.Id == post.Id);
                     
                     newPost.Updated = DateTime.Now;
                     newPost.Title = post.Title;
@@ -147,6 +172,21 @@ namespace TheITBlog.Controllers
                         newPost.ContentType = _imageService.ContentType(newImage);
                     }
 
+                    //Remove all Tags previously associated with this post
+                    _context.Tags.RemoveRange(newPost.Tags);
+
+                    await _context.SaveChangesAsync();
+
+                    //Add in the new Tags from the Edit form
+                    foreach(var tagText in tagValues)
+                    {
+                        _context.Add(new Tag()
+                        {
+                            PostId = post.Id,
+                            BlogUserId = newPost.BlogUserId,
+                            Text = tagText
+                        });
+                    }
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
---
+++ wwwroot/js/Custom.js
@@ -0,0 +1,95 @@
+﻿let index = 0;
+
+function AddTag() {
+    //Get a reference to the TagEntry input element
+    var tagEntry = document.getElementById("TagEntry");
+
+    //Use the new Search function to detect an error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        //Trigger the sweet alert for whatever condition is contained in the searchResult variable
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`
+        });
+    }
+    else {
+
+        //Create a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+
+    }
+
+    //Clear out the TagEntry control
+    tagEntry.value = "";
+    return true;
+}
+
+function DeleteTag() {
+    let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: `<span class='font-weight-bolder'>CHOOSE A TAG BEFORE DELETING</span>`
+        });
+        return true;
+    }
+
+    while (tagCount > 0) {
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
+            --tagCount;
+        }
+        else
+            tagCount = 0;
+        index--;
+    }
+}
+
+
+$("form").on("submit", function () {
+    $("#TagList option").prop("selected", "selected");
+})
+
+//Look for the tagValues variable to see if it has data
+if (tagValues != '') {
+    let tagArray = tagValues.split(",");
+    for (let loop = 0; loop < tagArray.length; loop++) {
+        //Load up or replace the options we have
+        ReplaceTag(tagArray[loop], loop);
+        index++;
+    }
+}
+
+function ReplaceTag(tag, index) {
+    let newOption = new Option(tag, tag);
+    document.getElementById("TagList").options[index] = newOption;
+}
+
+//This Search function will detect either an empty or duplicate Tag for this Post
+//and return an error string if an error is detected
+function search(str) {
+    if (str == "") {
+        return 'Empty tags are not allowed';
+    }
+
+    var tagsEl = document.getElementById('TagList');
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str)
+                return `The Tag #${str} was detected as a Duplicate and not allowed`
+        }
+    }
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-lg btn-outline-dark'
+    },
+    imageUrl: '/images/oops.jpg',
+    timer: 5000,
+    buttonsStyling: false
+})
